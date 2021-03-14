ed75e1d0eb1905d43d9423d5d4882a5fcc8a0596
izifaut <79901531+izifaut@users.noreply.github.com>
2021-03-14 13:23:45 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!(Array.isArray(array)) || !(array.length)){
+    return 0;
+  }
+  let min = array[0];
+  for (let i=0; i < array.length; i++){
+    if (min > array[i]) min = array[i];
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!(Array.isArray(array)) || !(array.length)){
+    return 0;
+  }
+  let max = array[0];
+  for (let i = 1; i < array.length; ++i){
+    if (max < array[i]) max = array[i];
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!(Array.isArray(array)) || !(array.length)){
+    return 0;
+  }
+  let sum = 0;
+  for (let i=0; i < array.length; i++){
+    sum = sum + array[i];
+  }
+  let avg = sum/array.length;
+  return avg;
 }
